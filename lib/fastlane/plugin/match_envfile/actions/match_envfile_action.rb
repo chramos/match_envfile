b4892c89513a8b4d352781f1b3d1ed5528b1893f
Henrique Ramos <henrique@v-thru.com>
2025-04-20 09:10:48 -0400

    add logic to fetch repository using basic auth and pull|push envfiles

--- lib/fastlane/plugin/match_envfile/actions/match_envfile_action.rb
+++ lib/fastlane/plugin/match_envfile/actions/match_envfile_action.rb
@@ -1,11 +1,78 @@
 require 'fastlane/action'
 require_relative '../helper/match_envfile_helper'
+require 'tmpdir'
+require 'openssl'
+require 'digest'
+require 'fileutils'
 
 module Fastlane
   module Actions
     class MatchEnvfileAction < Action
       def self.run(params)
-        UI.message("The match_envfile plugin is working! #{params}")
+        repo_url = params[:git_url]
+        repo_branch = params[:git_branch]
+        repo_basic_authorization = Base64.decode64(params[:git_basic_authorization])
+        environment = params[:environment]
+        readonly = params[:readonly]
+        password = params[:password]
+
+        project_root = Dir.pwd
+
+        if project_root.include?("ios")
+          project_root = project_root.gsub("/ios", "")
+        end
+
+        if project_root.include?("android")
+          project_root = project_root.gsub("/android", "")
+        end
+        
+        split_repo_url = repo_url.split("https://")
+        repo_url = split_repo_url[1]
+        git_url = "https://#{repo_basic_authorization}@#{repo_url}"
+
+        Dir.mktmpdir do |dir|
+          system("git clone #{git_url} #{dir}", chdir: dir)
+          system("cd #{dir} && git checkout #{repo_branch}", chdir: dir)
+          if readonly
+            envfile = File.read(dir + "/secrets/.env." + environment)
+            decoded = Base64.strict_decode64(envfile)
+            iv = decoded[0..15]                        # First 16 bytes = IV
+            encrypted = decoded[16..-1]                # Rest = encrypted content
+
+            cipher = OpenSSL::Cipher.new('aes-256-cbc')
+            cipher.decrypt
+            cipher.key = Digest::SHA256.digest(password)
+            cipher.iv = iv
+
+            envfile = cipher.update(encrypted) + cipher.final
+            File.write(project_root + "/.env." + environment, envfile)
+            FileUtils.rm_rf(dir)
+          end
+
+            envfile = File.read(project_root + "/.env." + environment)
+            cipher = OpenSSL::Cipher.new('aes-256-cbc')
+            cipher.encrypt
+
+            key = Digest::SHA256.digest(password) # derive key from password
+            iv = cipher.random_iv
+
+            cipher.key = key
+            cipher.iv = iv
+
+            encrypted = cipher.update(envfile) + cipher.final
+
+            encrypted_envfile = Base64.strict_encode64(iv + encrypted)
+
+            FileUtils.mkdir_p(dir + '/secrets')
+
+            File.write(dir + '/secrets/.env.' + environment, encrypted_envfile)
+
+            system("git add .", chdir: dir)
+            system("git commit -m 'Add .env.#{environment}'", chdir: dir)
+            system("git push origin #{repo_branch}", chdir: dir)
+            FileUtils.rm_rf(dir)
+        end
+        
       end
 
       def self.description
@@ -27,11 +94,37 @@ module Fastlane
 
       def self.available_options
         [
-          # FastlaneCore::ConfigItem.new(key: :your_option,
-          #                         env_name: "MATCH_ENVFILE_YOUR_OPTION",
-          #                      description: "A description of your option",
-          #                         optional: false,
-          #                             type: String)
+          FastlaneCore::ConfigItem.new(key: :environment  ,
+                                  env_name: "MATCH_ENVFILE_ENVIRONMENT",
+                               description: "Defines the environment to be used e.g. 'development', 'staging', 'production'",
+                                  optional: true,
+                                      type: String),
+          FastlaneCore::ConfigItem.new(key: :readonly,
+                                  env_name: "MATCH_ENVFILE_READONLY",
+                               description: "Defines if you want to read or write to the .env file",
+                                  optional: true,
+                                      type: Boolean),
+          FastlaneCore::ConfigItem.new(key: :password,
+                                  env_name: "MATCH_PASSWORD",
+                               description: "The password to be used to encrypt/decrypt the .env file",
+                                  optional: false,
+                                      type: String),
+          FastlaneCore::ConfigItem.new(key: :git_url,
+                                  env_name: "MATCH_GIT_URL",
+                               description: "The URL of the GitHub repository to be used to store the .env file",
+                                  optional: false,
+                                      type: String),
+          FastlaneCore::ConfigItem.new(key: :git_basic_authorization,
+                                  env_name: "MATCH_GIT_BASIC_AUTHORIZATION",
+                               description: "The basic authorization to be used to access the GitHub repository",
+                                  optional: true,
+                                      type: String),
+          FastlaneCore::ConfigItem.new(key: :git_branch,
+                                  env_name: "MATCH_GIT_BRANCH",
+                               description: "The branch to be used to store or read the .env file",
+                                  optional: true,
+                                      type: String)
+
         ]
       end
 
